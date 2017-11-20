4afb2934b401fcdf38784fc51ac772de55016dfe
alberto56 <albert@mediatribe.net>
2017-11-20 09:01:01 -0500

    Please squash this commit in the pull request

--- docker-resources/run-tests.sh
+++ docker-resources/run-tests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# This is the entrypoint to the container.
+# We need to start Chrome in the background, then run our tests.
+# Once this script exists, the container will be destroyed.
+#
 echo -e '[info] Starting chrome in the background.'
 # https://unix.stackexchange.com/questions/103731
 /scripts/start-chrome.sh > output.log 2>&1 &
--- example01/test/test.js
+++ example01/test/test.js
@@ -16,7 +16,6 @@ describe('When searching on google', function () {
       .type('chromeless github', 'input[name="q"]')
       .press(13) // press enter
       .wait('#resultStats')
-      .screenshot()
 
     const result = await chromeless.exists('a[href*="graphcool/chromeless"]')
 
@@ -24,61 +23,3 @@ describe('When searching on google', function () {
     await chromeless.end()
   })
 })
-
-describe('When clicking on the image of the demo playground', function () {
-  it('should redirect to the demo', async function () {
-    this.timeout(10000); //we need to increase the timeout or else mocha will exit with an error
-    const chromeless = new Chromeless()
-    await chromeless.goto('https://github.com/graphcool/chromeless')
-      .wait('a[href="https://chromeless.netlify.com/"]')
-      .click('a[href="https://chromeless.netlify.com/"]')
-      .wait('#root')
-
-
-    const url = await chromeless.evaluate(url => window.location.href)
-
-
-    expect(url).to.match(/^https\:\/\/chromeless\.netlify\.com/)
-    await chromeless.end()
-  })
-})
-
-// See https://github.com/graphcool/chromeless/tree/master/examples
-//
-// async function run() {
-//   const chromeless = new Chromeless()
-//
-//   const screenshot = await chromeless
-//     .goto('https://www.google.com')
-//     .type('javascript', 'input[name="q"]')
-//     .press(13)
-//     .wait('#resultStats')
-//     .screenshot()
-//
-//   testsManager.assert(document.title.length);
-//
-//   console.log(screenshot) // prints local file path or S3 url
-//
-//   await chromeless.end()
-// }
-//
-// run().catch(console.error.bind(console))
-//
-//
-//
-// casper.test.begin('Test Google.com', 2, function(test) {
-//   casper.start('http://google.com', function() {
-//     this.fill('form[action="/search"]', {
-//       'q': 'javascript'
-//     }, true);
-//   });
-//
-//   casper.then(function() {
-//     test.assertTitleMatch(/^.*javascript.*$/, 'Google search results page doesnt have expected title');
-//     test.assertTitleMatch(/^.*Google.*$/, 'Google search results page doesnt have expected title');
-//   });
-//
-//   casper.run(function() {
-//     test.done();
-//   });
-// });
