a84411a18eb12e936d72885cce6e1054adf28b8b
alberto56 <albert@mediatribe.net>
2019-03-27 11:08:32 -0400

    Please squash this commit in the pull request

--- example01/test/test.js
+++ example01/test/test.js
@@ -1,28 +1,23 @@
-const puppeteer = require('puppeteer')
-const screenshot = 'amazon_nyan_cat_pullover.png'
 (async () => {
+  const puppeteer = require('puppeteer')
   const browser = await puppeteer.launch({
      headless: true,
      args: ['--no-sandbox', '--disable-setuid-sandbox']
   })
+  var result = false
   try {
     const page = await browser.newPage()
     await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('https://www.amazon.com')
-    await page.type('#twotabsearchtextbox', 'nyan cat pullover')
-    await page.click('input.nav-input')
-    await page.waitForSelector('#resultsCol')
-    await page.screenshot({path: 'amazon_nyan_cat_pullovers_list.png'})
-    await page.click('#pagnNextString')
-    await page.waitForSelector('#resultsCol')
-    const pullovers = await page.$$('a.a-link-normal.a-text-normal')
-    await pullovers[2].click()
-    await page.waitForSelector('#ppd')
-    await page.screenshot({path: screenshot})
+    await page.goto('https://google.com')
+    await page.type('input[name="q"]', 'puppeteer')
+    // await page.type(String.fromCharCode(13));
+    // await page.waitForSelector('#resultStats')
+    // await page.screenshot()
+    // await page.waitForSelector('a[href*="graphcool/chromeless"]')
+    result = true
   }
   catch (error) {
     console.log(error);
   }
   await browser.close()
-  console.log('See screenshot: ' + screenshot)
 })()
