b6a225ca58ab164ee6e56480649aed2856a9fd9d
Albert Albala <albert@mediatribe.net>
2022-04-21 11:35:43 -0400

    Please squash this commit in the pull request

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -20,8 +20,8 @@ fi
 
 PROJECT=browsertesting
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
-MAJORVERSION='3'
-VERSION='3.0'
+MAJORVERSION='4'
+VERSION='4.0'
 
 # See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
 git clone https://github.com/dcycle/prepare-docker-buildx.git
@@ -35,7 +35,7 @@ docker buildx use mybuilder
 docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
 
-docker pull node
+docker pull node:alpine
 
 docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
 docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
--- test-from-dockerhub-image.sh
+++ test-from-dockerhub-image.sh
@@ -12,13 +12,13 @@ echo '=> Build the sample app'
 docker run --rm \
   -v "$(pwd)"/example01/test:/app/test \
   -v "$(pwd)"/artifacts:/artifacts \
-  dcycle/browsertesting:3 test/*.js
+  dcycle/browsertesting:4 test/*.js
 
 docker run --rm \
   -v "$(pwd)"/example02/test:/app/test \
   -v "$(pwd)"/artifacts:/artifacts \
   --network myapp-network \
-  dcycle/browsertesting:3 test/*.js
+  dcycle/browsertesting:4 test/*.js
 
 echo '=> Destroy the sample app'
 (cd ./example02 && ./destroy-sample-app.sh)
