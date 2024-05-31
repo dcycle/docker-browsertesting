198dedf27e2704fc06a1ef63ee33780229a2b55d
Albert Albala <albert@mediatribe.net>
2024-05-31 18:05:26 -0400

    Please squash this commit in the pull request

--- ci.sh
+++ ci.sh
@@ -8,26 +8,26 @@ set -e
 echo '=> Build the image locally and test'
 ./test-after-building-image.sh
 
-# echo '=> Fail if a selector is not found.'
-# docker run --rm \
-#   -v "$(pwd)"/example01/selector-not-present-example:/app/test \
-#   local-browsertesting test/*.js && FAIL=0 || FAIL=1
-# if [ $FAIL == 0 ]; then
-#   echo 'Previous call was expected to fail but did not'
-#   exit 1
-# else
-#   echo 'Previous call failed as expected'
-# fi
+echo '=> Fail if a selector is not found.'
+docker run --rm \
+  -v "$(pwd)"/example01/selector-not-present-example:/app/test \
+  local-browsertesting test/*.js && FAIL=0 || FAIL=1
+if [ $FAIL == 0 ]; then
+  echo 'Previous call was expected to fail but did not'
+  exit 1
+else
+  echo 'Previous call failed as expected'
+fi
 
-# echo '=> Fail if a chai assertion fails.'
-# docker run --rm \
-#   -v "$(pwd)"/example01/failing-test-example:/app/test \
-#   local-browsertesting test/*.js && FAIL=0 || FAIL=1
-# if [ $FAIL == 0 ]; then
-#   echo 'Previous call was expected to fail but did not'
-#   exit 1
-# else
-#   echo 'Previous call failed as expected'
-# fi
+echo '=> Fail if a chai assertion fails.'
+docker run --rm \
+  -v "$(pwd)"/example01/failing-test-example:/app/test \
+  local-browsertesting test/*.js && FAIL=0 || FAIL=1
+if [ $FAIL == 0 ]; then
+  echo 'Previous call was expected to fail but did not'
+  exit 1
+else
+  echo 'Previous call failed as expected'
+fi
 
 echo '=> Testing complete!'
--- test-after-building-image.sh
+++ test-after-building-image.sh
@@ -6,14 +6,14 @@ set -e
 
 docker build -t local-browsertesting .
 
-# echo '=> Build the sample app'
-# (cd ./example02 && ./build-sample-app.sh)
+echo '=> Build the sample app'
+(cd ./example02 && ./build-sample-app.sh)
 
-# echo '=> Test example01'
-# docker run --rm \
-#   -v "$(pwd)"/example01/test:/app/test \
-#   -v "$(pwd)"/artifacts:/artifacts \
-#   local-browsertesting test/*.js
+echo '=> Test example01'
+docker run --rm \
+  -v "$(pwd)"/example01/test:/app/test \
+  -v "$(pwd)"/artifacts:/artifacts \
+  local-browsertesting test/*.js
 
 echo '=> Test example02'
 docker run --rm -v "$(pwd)"/example02/test:/app/test \
@@ -21,5 +21,5 @@ docker run --rm -v "$(pwd)"/example02/test:/app/test \
   --network myapp-network \
   local-browsertesting test/*.js
 
-# echo '=> Destroy the sample app'
-# (cd ./example02 && ./destroy-sample-app.sh)
+echo '=> Destroy the sample app'
+(cd ./example02 && ./destroy-sample-app.sh)
