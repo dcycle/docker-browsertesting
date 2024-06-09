12658133b83e8b5e12551f2fb04bc255318187d6
Albert Albala <albert@mediatribe.net>
2024-06-09 18:38:38 -0400

    Please squash this commit in the pull request

--- test-after-building-image.sh
+++ test-after-building-image.sh
@@ -10,6 +10,10 @@ echo '=> Build the sample app'
 (cd ./example02 && ./build-sample-app.sh)
 
 echo '=> Test example01'
+echo '=> This tests that is possible to make a request in Google. If this'
+echo '=> fails, it might mean that you are using a european connection and'
+echo '=> that the test is stuck on the Accept Cookies page'
+echo '=> If this is the case, make sure you are using a US-based vpn or VM'
 docker run --rm \
   -v "$(pwd)"/example01/test:/app/test \
   -v "$(pwd)"/artifacts:/artifacts \
