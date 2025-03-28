465dbc9725e22c6b02c43d167f7b8ea46b0b95f9
root <suresh@vmi601282.junkwebhosting.net>
2025-03-28 18:35:36 +0100

    test fix2

--- example02/test/test.mjs
+++ example02/test/test.mjs
@@ -1,14 +1,14 @@
-const puppeteer = require('puppeteer');
-const { expect } = require('chai');
-const fs = require('fs');
+import puppeteer from 'puppeteer';
+import { expect } from 'chai';
+import fs from 'fs';
 
 it('It should be possible to add a "todo" item to our app', async function() {
-  this.timeout(40000);
+  this.timeout(80000);
   const browser = await puppeteer.launch({
      headless: true,
      args: ['--no-sandbox', '--disable-setuid-sandbox'],
      // Adjust the protocolTimeout for script loading
-     protocolTimeout: 40000
+     protocolTimeout: 100000
   })
   let result = false
   try {
