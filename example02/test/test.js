ca124e2c890933caaebe5e4518a9d77da4389ecf
Albert Albala <albert@mediatribe.net>
2025-03-30 20:07:32 -0400

    Fix tests: Do not test the example with a search on Google, because it does not pass consistently (#21)

--- example02/test/test.js
+++ example02/test/test.js
@@ -1,12 +1,13 @@
-import puppeteer from 'puppeteer';
-import { expect } from 'chai';
-import fs from 'fs';
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const fs = require('fs');
 
 it('It should be possible to add a "todo" item to our app', async function() {
-  this.timeout(15000);
+  this.timeout(80000);
   const browser = await puppeteer.launch({
      headless: true,
-     args: ['--no-sandbox', '--disable-setuid-sandbox']
+     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+     protocolTimeout: 100000  // Adjust this value as needed for your app's load time
   })
   let result = false
   try {
--- test-after-building-image.sh
+++ test-after-building-image.sh
@@ -9,21 +9,11 @@ docker build -t local-browsertesting .
 echo '=> Build the sample app'
 (cd ./example02 && ./build-sample-app.sh)
 
-echo '=> Test example01'
-echo '=> This tests that is possible to make a request in Google. If this'
-echo '=> fails, it might mean that you are using a european connection and'
-echo '=> that the test is stuck on the Accept Cookies page'
-echo '=> If this is the case, make sure you are using a US-based vpn or VM'
-docker run --rm \
-  -v "$(pwd)"/example01/test:/app/test \
-  -v "$(pwd)"/artifacts:/artifacts \
-  local-browsertesting test/*.mjs
-
 echo '=> Test example02'
 docker run --rm -v "$(pwd)"/example02/test:/app/test \
   -v "$(pwd)"/artifacts:/artifacts \
   --network myapp-network \
-  local-browsertesting test/*.mjs
+  local-browsertesting test/*.js
 
 echo '=> Destroy the sample app'
 (cd ./example02 && ./destroy-sample-app.sh)
